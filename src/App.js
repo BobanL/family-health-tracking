c3a04585ce90157538966bbbe3ba66b23b4eed49
Boban <ljuljdjurovic@gmail.com>
2020-12-01 10:22:25 -0500

    feat: make header a custom component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Dashboard from './Dashboard';
+import "./App.css";
+import { Dashboard, useStyles } from "./Dashboard";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Header } from "./Header";
+import { CssBaseline } from "@material-ui/core";
 function App() {
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Dashboard}></Route>
-      </Switch>
+      <div className={classes.root}>
+        <CssBaseline />
+        <Header></Header>
+        <Switch>
+          <Route path="/" exact component={Dashboard}></Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
