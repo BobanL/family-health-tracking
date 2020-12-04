e980aac939874d6a20f19b5ac63c046a8234b888
Boban <ljuljdjurovic@gmail.com>
2020-12-04 16:12:29 -0500

    feat: add basic medical records retrieval

--- src/TablePage.js
+++ src/TablePage.js
@@ -10,17 +10,6 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
 export default class TablePage extends React.Component {
   state = {
     loading: true,
@@ -44,36 +33,37 @@ export default class TablePage extends React.Component {
           <Grid container>
             <Grid item xs={12}>
               <Paper className={classes.paper}>
-                {this.state.loading || !this.state.formData ? (
-                  <div>loading...</div>
-                ) : (
-                  <TableContainer component={Paper}>
-                    <Table className={classes.table} aria-label="simple table">
-                      <TableHead>
-                        <TableRow>
-                          <TableCell>Dessert (100g serving)</TableCell>
-                          <TableCell align="right">Calories</TableCell>
-                          <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                          <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                          <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {rows.map((row) => (
-                          <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                              {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
+                {
+                  this.state.loading || !this.state.formData ? (
+                    <div>loading...</div>
+                  ) : (
+                    <TableContainer component={Paper}>
+                      <Table
+                        className={classes.table}
+                        aria-label="simple table"
+                      >
+                        <TableHead>
+                          <TableRow>
+                            {Object.keys(this.state.formData[0]).map((key) => (
+                              <TableCell>{key}</TableCell>
+                            ))}
                           </TableRow>
-                        ))}
-                      </TableBody>
-                    </Table>
-                  </TableContainer>
-                )}
+                        </TableHead>
+                        <TableBody>
+                          {this.state.formData.map((row) => (
+                            <TableRow>
+                              {Object.keys(row).map((key) => (
+                                <TableCell component="th" scope="row">
+                                  {row[key]}
+                                </TableCell>
+                              ))}
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  )
+                }
               </Paper>
             </Grid>
           </Grid>
